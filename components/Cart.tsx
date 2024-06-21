4e2cba0834aad2db2b34b313f46f1ebff6ce43bb
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-21 16:05:47 +0530

    Component Structure Component Declaration: Cart is a functional component that renders the shopping cart page.

---
+++ components/Cart.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import ShoppingCart from './ShoppingCart'
+import ProccedToBuy from './ProccedToBuy'
+import { useAppSelector } from '@/lib/supabase/hooks/redux';
+import { getCart } from '@/redux/cartSlice';
+
+const Cart = () => {
+    
+    
+    
+}
+
