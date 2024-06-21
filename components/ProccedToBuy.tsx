b321235a1cc4e04977f504c3bcfb371cfc8c067e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-21 16:12:15 +0530

    Component Structure Component Declaration: ProccedToBuy is a functional component that receives props { length, totalPrice }. length: Represents the number of items in the cart. totalPrice: Represents the total price of all items in the cart.

---
+++ components/ProccedToBuy.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Subtotal from './shared/Subtotal';
+import { useRouter } from 'next/navigation';
+
+const ProccedToBuy = ({ length, totalPrice }: { length: number, totalPrice: number }) => {
+    
+    
+}
+
