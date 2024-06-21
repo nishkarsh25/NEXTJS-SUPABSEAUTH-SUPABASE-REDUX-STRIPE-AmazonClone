c80fde5f3b8363a06d329e705aedd742514e5d8b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-21 16:22:49 +0530

    Component Structure Component Declaration: DeliveryAddress is a functional component that doesn't receive any props directly from its parent components.

---
+++ components/DeliveryAddress.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useAppSelector } from '@/lib/supabase/hooks/redux';
+import { getCart } from '@/redux/cartSlice';
+import Image from "next/image";
+
+const DeliveryAddress = () => {
+    
+    
+}
+
