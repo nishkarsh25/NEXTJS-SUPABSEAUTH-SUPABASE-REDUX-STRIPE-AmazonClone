97ccf3f2f25d8a518d4caf17e9e646eef7a6e9dd
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-21 16:35:54 +0530

    Component Overview Component Declaration: SingleProduct is a functional component responsible for displaying details of a single product. Props Props: singleProduct: Object containing the details of a single product. This prop is expected to have properties such as id, title, description, image, price, rating, etc.

---
+++ components/SingleProduct.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import React from 'react'
+import Ratings from './shared/Ratings';
+import AddToCardContainer from './AddToCardContainer';
+
+const SingleProduct = ({ singleProduct }: { singleProduct: any }) => {
+    
+    
+}
+
