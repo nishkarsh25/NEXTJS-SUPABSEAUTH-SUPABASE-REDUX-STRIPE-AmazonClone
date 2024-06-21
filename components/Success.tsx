33b8423f5148a30b262b700d2946264ff3ac4b09
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-21 10:53:46 +0530

    Imports Redux Selector Hook:
    useAppSelector is imported from @/lib/supabase/hooks/redux. This custom hook is likely a wrapper around useSelector from Redux Toolkit or React-Redux, allowing components to select data from the Redux store.
    Redux Selector Function:
    
    getCart is imported from @/redux/cartSlice. This function is a selector that retrieves the cart state from the Redux store.
    React Components:
    
    React is imported to enable the use of JSX and React components.
    Image is imported from next/image. It is used to optimize the loading and rendering of images in Next.js applications.
    Link is imported from next/link. It enables client-side navigation between pages in a Next.js application.

---
+++ components/Success.tsx
@@ -0,0 +1,8 @@
+"use client"
+import { useAppSelector } from '@/lib/supabase/hooks/redux'
+import { getCart } from '@/redux/cartSlice'
+import React from 'react';
+import Image from "next/image";
+import Link from 'next/link';
+
+
