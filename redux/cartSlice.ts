3ee6a261aee876eab9f58bb7d5cb07d1c8359a1c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-21 09:03:40 +0530

    Interface Definition CartState Interface: Defines the structure of the state for the cart. It includes a cart property, which is an array. The any type is used here, but it could be more strictly typed to improve type safety.

--- redux/cartSlice.ts
+++ redux/cartSlice.ts
@@ -1,6 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "./index";
 
+interface CartSate {
+    cart: any,
+}
 
+const initialState:CartSate = {
+    cart:[]
+}
 
 
