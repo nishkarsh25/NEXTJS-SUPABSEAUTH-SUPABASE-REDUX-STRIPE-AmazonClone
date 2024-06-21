29e41134d6811ff99f60f1162cf5c8b1f4688043
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-21 09:04:55 +0530

    Reducers (Actions) addToCart Action:
    This action checks if the item (identified by id) is already in the cart.
    If the item is present, it updates the item's quantity by incrementing it by 1.
    If the item is not present, it adds the item to the cart with an initial quantity of 1.
    removeFromTheCart Action:
    
    This action removes an item from the cart based on its id.
    It filters out the item with the specified id from the cart array.
    incrementQuantity Action:
    
    This action increments the quantity of a specific item in the cart.
    It finds the item by id and increases its quantity by 1.
    decrementQuantity Action:
    
    This action decrements the quantity of a specific item in the cart.
    It finds the item by id and decreases its quantity by 1.
    clearAllCart Action:
    This action clears all items from the cart, setting the cart array to an empty array.

--- redux/cartSlice.ts
+++ redux/cartSlice.ts
@@ -11,7 +11,39 @@ const initialState:CartSate = {
 
 const cartSlice = createSlice({
     name:"cart",
-    
     initialState,
-    
+    reducers:{
+        // multiple actions
+        addToCart:(state, action)=>{
+            const isPresent = state.cart.find((item:any)=>{
+                return item.id === action.payload.id;
+            })
+            if(isPresent){
+                // update quanity na ki add item
+                state.cart = state.cart.map((item:any)=>{
+                    return item.id === action.payload.id ? {...item, quantity:item.quantity+1} : item;
+                })
+            }else{
+                state.cart.push({...action.payload, quantity:1});
+            }
+        },
+        removeFromTheCart:(state, action)=>{
+            state.cart = state.cart.filter((item:any)=>{
+                return item.id !== action.payload;
+            })
+        },
+        incrementQuantity:(state,action)=>{
+            state.cart = state.cart.map((item:any)=>{
+                return item.id === action.payload.id ? {...item, quantity:item.quantity+1} : item;
+            })
+        },
+        decrementQuantity:(state,action)=>{
+            state.cart = state.cart.map((item:any)=>{
+                return item.id === action.payload.id ? {...item, quantity:item.quantity-1} : item;
+            })
+        },
+        clearAllCart:(state)=>{
+            state.cart = []
+        }
+    }
 });
