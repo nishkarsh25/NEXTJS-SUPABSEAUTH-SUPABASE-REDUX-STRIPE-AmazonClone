9bab0fbc4dbdbe879a72e7265109f82a3333b812
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-21 16:34:37 +0530

    Component Overview Component Declaration: ProductPage is a functional component responsible for displaying a single product page based on the id parameter extracted from the URL using useParams().

---
+++ app/product/[id]/page.tsx
@@ -0,0 +1,13 @@
+"use client" 
+import SingleProduct from '@/components/SingleProduct'
+import { useSupabase } from '@/lib/supabase/hooks/useSupabase'
+import { useParams } from 'next/navigation'
+import React, { useEffect } from 'react'
+
+const ProductPage = () => {
+    
+    
+   
+    
+}
+
