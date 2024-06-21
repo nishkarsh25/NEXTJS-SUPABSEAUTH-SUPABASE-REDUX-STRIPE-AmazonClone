e7a8a42cc248c142e033faf5314bbfd4f06d1487
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-21 16:37:18 +0530

    Component Overview Component Declaration: AddToCardContainer is a functional component responsible for displaying options related to adding a product to the cart and delivery details. Props Props: product: Object containing the details of the product to be added to the cart. This prop typically includes properties like id, title, image, price, etc.

---
+++ components/AddToCardContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import prime from "../public/prime-logo.png";
+import Image from "next/image";
+import { useAppDispatch } from '@/lib/supabase/hooks/redux';
+import { addToCart } from '@/redux/cartSlice';
+import { useRouter } from 'next/navigation';
+
+const AddToCardContainer = ({ product }: { product: any }) => {
+    
+    
+}
+
