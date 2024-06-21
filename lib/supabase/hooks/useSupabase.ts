41e8039f4d08ec87ac1f351098bbeda9bb3d4a02
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-21 08:12:15 +0530

    State Management State Variables:
    products: Stores all products fetched from the Supabase database.
    filterData: Stores filtered products based on a search query.
    singleProduct: Stores details of a single product fetched by its ID.
    mensProduct: Stores products categorized as men's clothing.
    womensProduct: Stores products categorized as women's clothing.
    Each state variable is initialized using the useState hook and starts with an empty array.

--- lib/supabase/hooks/useSupabase.ts
+++ lib/supabase/hooks/useSupabase.ts
@@ -2,4 +2,19 @@ import { useState } from "react";
 import { supabase } from "../products";
 
 
+export const useSupabase = () => {
+  const [products, setProducts] = useState<any>([]);
+  const [filterData, setFilterData] = useState<any>([]);
+  const [singleProduct, setSingleProduct] = useState<any>([]);
+  const [mensProduct, setMensProduct] = useState<any>([]);
+  const [womensProduct, setWomensProduct] = useState<any>([]);
 
+  
+  
+
+  
+  
+  
+
+  
+};
