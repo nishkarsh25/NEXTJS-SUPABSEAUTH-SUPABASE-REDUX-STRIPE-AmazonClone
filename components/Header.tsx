5fe8bfd26538ac1ca369ad998ecf51ffadd08606
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-21 15:33:32 +0530

    Imports Next.js Image Component:
    The Image component from next/image is imported for optimized image handling. It's used to display the Amazon logo.
    React Icons:
    
    Icons (BiCart and CgSearch) are imported from their respective libraries (react-icons/bi and react-icons/cg) to use in the header for cart and search functionalities.
    Next.js Link Component:
    
    The Link component from next/link is imported to enable client-side navigation between pages.
    Next.js Router:
    
    The useRouter hook from next/navigation is imported. It provides methods for client-side navigation in a Next.js application.
    Redux Hooks:
    
    useAppSelector is imported from @/lib/supabase/hooks/redux to select data from the Redux store (cartSlice in this case).
    getCart is imported from @/redux/cartSlice to retrieve cart data specifically.
    Supabase Integration:
    
    supabase is imported from @/lib/supabase/products. This likely initializes or accesses Supabase functionality, such as authentication or database operations.

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+"use client"
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+import amazonLogo from "../public/amazon-logo-2.webp";
+import { BiCart } from "react-icons/bi";
+import { CgSearch } from "react-icons/cg";
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useAppSelector } from '@/lib/supabase/hooks/redux';
+import { getCart } from '@/redux/cartSlice';
+import { supabase } from '@/lib/supabase/products';
+
+
+
+
+
