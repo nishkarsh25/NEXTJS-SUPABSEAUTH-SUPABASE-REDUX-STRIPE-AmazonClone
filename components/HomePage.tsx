772102528c10af2f92eed64f261c4c95cbef9bf9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-21 15:13:14 +0530

    Imports React and Hooks:
    React is imported to enable the use of JSX and React components.
    The useEffect hook is imported from React. It allows side effects, such as fetching data, to be performed in functional components.
    Next.js Image Component:
    
    The Image component from next/image is imported for optimized image handling. It is used to display images with optimized performance and built-in responsiveness.
    Supabase Hook:
    
    The useSupabase custom hook is imported from @/lib/supabase/hooks/useSupabase. This hook likely contains logic for interacting with Supabase, a backend service.
    Shared Component:
    
    The CategoryWiseProduct component is imported from a shared directory. This component is used to display individual products within a category.
    Next.js Link Component:
    
    The Link component from next/link is imported to enable client-side navigation between pages in a Next.js application.

---
+++ components/HomePage.tsx
@@ -0,0 +1,9 @@
+"use client"
+import React, { useEffect } from 'react';
+import Image from "next/image";
+import { useSupabase } from '@/lib/supabase/hooks/useSupabase';
+import CategoryWiseProduct from './shared/CategoryWiseProduct';
+import Link from "next/link";
+
+
+
