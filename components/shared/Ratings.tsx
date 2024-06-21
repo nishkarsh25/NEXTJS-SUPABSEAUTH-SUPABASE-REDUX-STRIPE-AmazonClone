0382c7c8001a2f8e5677b64a886eb3c28719f17f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-21 15:27:10 +0530

    Certainly! Let's break down the Ratings component step-by-step to understand its structure and functionality in great detail:
    Imports
    React:
    
    React is imported to enable the use of JSX and React components.
    Next.js Image Component:
    
    The Image component from next/image is imported for optimized image handling. It helps to load and render images efficiently with built-in performance optimizations.
    Star Icon:
    
    The star icon image is imported from the public directory. This icon will be used to visually represent the product's rating.
    Ratings Component Declaration
    Component Declaration:
    The Ratings component is defined as a functional component. It accepts a single prop, ratings, which contains the rating details to be displayed.

--- components/shared/Ratings.tsx
+++ components/shared/Ratings.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import rating from "../../public/star-icon.png";
 import Image from "next/image";
 
-
+const Ratings = ({ratings}:{ratings:any}) => {
+    
+   
+    
+}
 
