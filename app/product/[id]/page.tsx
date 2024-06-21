13facb913b543989128e7c6050d71012db6b9938
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-21 16:34:45 +0530

    Hooks and State State and Hooks: useParams() from next/navigation: Destructures the id parameter from the URL path to identify which product to fetch. useSupabase() Custom Hook: Destructures singleProduct and getSingleProduct functions from the useSupabase() hook. singleProduct: Holds the state of the fetched product data. getSingleProduct: Function used to fetch a single product from a data source (likely Supabase based on the hook name).

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -5,7 +5,8 @@ import { useParams } from 'next/navigation'
 import React, { useEffect } from 'react'
 
 const ProductPage = () => {
-    
+    const { id } = useParams();
+    const { singleProduct, getSingleProduct } = useSupabase();
     
    
     
