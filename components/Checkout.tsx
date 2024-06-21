3f01c18216e5e694f076072d738a806af19de16a
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-21 09:47:54 +0530

    JSX Structure Main Container:
    A div container with specific styles is used to wrap the entire checkout page content. It ensures the content is positioned correctly and styled appropriately.
    Header Section:
    
    A div for the header section is created, containing:
    The Amazon logo displayed using the Image component.
    A "Checkout" title displayed as a heading.
    The FaLock icon indicating the secure nature of the checkout page.
    Main Content Section:
    
    A div is used to layout the main content of the checkout page. It contains:
    The DeliveryAddress component, which handles the user's delivery address information.
    The OrderSummary component, which displays the summary of the order, including the total price calculated earlier.

--- components/Checkout.tsx
+++ components/Checkout.tsx
@@ -17,6 +17,24 @@ const Checkout = () => {
     });
 
 
-    
+    return (
+        <div className='absolute top-0 w-full p-10  bg-white'>
+            <div className=' flex w-[70%] mx-auto items-center border-b border-gray-400  pb-5 justify-between'>
+                <div>
+                    <Image src={amazonLogo} alt={"amazon-logo"} width={150} height={150} />
+                </div>
+                <div>
+                    <h1 className='font-bold text-2xl'>Checkout</h1>
+                </div>
+                <div className='text-gray-400'>
+                    <FaLock size={"30px"} />
+                </div>
+            </div>
+            <div className='flex justify-between w-[70%] mx-auto'>
+                <DeliveryAddress />
+                <OrderSummary totalPrice={totalPrice} />
+            </div>
+        </div>
+    )
 }
 
